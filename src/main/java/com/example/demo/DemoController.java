1d9e06cbc715c7986ea604370918f7c376146a1d
Gareth Park <garethpark@yahoo.com>
2024-09-16 20:36:57 +0100

    added controller

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, OpenShift and Helm!";
+    }
+}
